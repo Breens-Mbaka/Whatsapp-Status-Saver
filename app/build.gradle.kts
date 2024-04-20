54b7253c987c38cb722045f33decb895a29671f2
breensmbaka <breens.mbaka@dlight.com>
2024-04-20 16:37:02 +0300

    Feat: Update gradle versionName to 1.2 and versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.breens.whatsappstatussaver"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
